905dfae3d1d2e8562e7f1252d8c7c2305296fe08
DjairAlmeida90 <djaptbr10@gmail.com>
2022-12-15 12:14:11 +0000

    studying reactJS CRUD

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import TernaryO from "./TernaryO";
 import App from "./App";
 import State from "./State";
+import Crud from "./Crud";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <TernaryO />
     <App />
     <State />
+    <Crud />
   </React.StrictMode>
 );
